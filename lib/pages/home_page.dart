bfad54ec05d119170420ad6506ff118fa6c097b8
MubasherAli110 <mubasheralibangash@gmail.com>
2023-09-13 15:38:40 +0500

    ad

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,113 +1,5 @@
-// import 'package:flutter/material.dart';
-// import 'package:google_mobile_ads/google_mobile_ads.dart';
-
-// class HomePage extends StatefulWidget {
-//   const HomePage({super.key});
-
-//   @override
-//   State<HomePage> createState() => _HomePageState();
-// }
-
-// class _HomePageState extends State<HomePage> {
-//   late BannerAd _bannerAd;
-
-//   @override
-//   void initState() {
-//     super.initState();
-
-//     _bannerAd = BannerAd(
-//       adUnitId: 'ca-app-pub-3940256099942544/6300978111',
-//       size: AdSize.banner,
-//       request: const AdRequest(),
-//       listener: const BannerAdListener(),
-//     );
-
-//     _bannerAd.load();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return AdWidget(ad: _bannerAd);
-//   }
-
-//   @override
-//   void dispose() {
-//     _bannerAd.dispose();
-//     super.dispose();
-//   }
-// }
-
-// @override
-// Widget build(BuildContext context) {
-//   return Scaffold(
-//     appBar: AppBar(
-//       title: const Text('Banner Ad Example'),
-//     ),
-//     body: const Center(
-//       child: Column(
-//         mainAxisAlignment: MainAxisAlignment.center,
-//         children: [
-//           Text(
-//             'Your App Content Goes Here',
-//             style: TextStyle(fontSize: 18),
-//           ),
-//           SizedBox(height: 20),
-//           BannerAdWidget(), // Display the banner ad here
-//         ],
-//       ),
-//     ),
-//   );
-// }
-
-// class BannerAdWidget extends StatefulWidget {
-//   const BannerAdWidget({super.key});
-
-//   @override
-//   // ignore: library_private_types_in_public_api
-//   _BannerAdWidgetState createState() => _BannerAdWidgetState();
-// }
-
-// class _BannerAdWidgetState extends State<BannerAdWidget> {
-//   late BannerAd _bannerAd;
-
-//   @override
-//   void initState() {
-//     super.initState();
-
-//     _bannerAd = BannerAd(
-//       adUnitId: 'ca-app-pub-1594286387548643~6666685047',
-//       size: AdSize.banner,
-//       request: const AdRequest(),
-//       listener: BannerAdListener(
-//         onAdLoaded: (Ad ad) {
-//           print('Banner Ad loaded.');
-//         },
-//         onAdFailedToLoad: (Ad ad, LoadAdError error) {
-//           // Ad failed to load
-//           print('Banner Ad failed to load: $error');
-//         },
-//         // Add other event listeners as needed
-//       ),
-//     );
-
-//     _bannerAd.load();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return AdWidget(ad: _bannerAd);
-//   }
-
-//   @override
-//   void dispose() {
-//     _bannerAd.dispose();
-//     super.dispose();
-//   }
-// }
-
 import 'package:blog/pages/banner_ad.dart';
 import 'package:flutter/material.dart';
-import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 import 'interstitial_ad.dart';
 
@@ -122,7 +14,7 @@ class _HomePageState extends State<HomePage> {
   TabBar get tabBar => const TabBar(
         indicator: UnderlineTabIndicator(
           borderSide: BorderSide(width: 1.0, color: Color(0xff15B9A5)),
-          insets: EdgeInsets.symmetric(horizontal: 10),
+          insets: EdgeInsets.symmetric(horizontal: 20),
         ),
         labelColor: Color(0xff15B9A5),
         labelStyle: TextStyle(
@@ -140,7 +32,9 @@ class _HomePageState extends State<HomePage> {
       length: 2,
       child: Scaffold(
           appBar: AppBar(
-            title: Text("Ad"),
+            backgroundColor: const Color(0xff15B9A5),
+            title: const Text("Ad Page"),
+            centerTitle: true,
             bottom: PreferredSize(
               preferredSize: tabBar.preferredSize,
               child: Material(
